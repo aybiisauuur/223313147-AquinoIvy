4460a500b20289730f043214d4a89323f0b1aee8
aybiisauuur <ivyaquino981@gmail.com>
2024-10-09 22:06:59 +0800

    loops 2

--- loops.php
+++ loops.php
@@ -15,4 +15,20 @@ while ($numeven <= 20) {
     echo $numeven, " ";
     $numeven += 2;
 }
+
+//activity 2
+$correctPass = "password123";
+$inputPass = "";
+
+do {
+    echo "Please enter your password: ";
+    $inputPass = trim(fgets(STDIN));
+    
+    // Check if the password is incorrect
+    if ($inputPass !== $correctPass) {
+        echo "Incorrect password. Try again.\n";
+    }
+} while ($inputPass !== $correctPass);
+
+echo "Access Granted.\n";
 ?>
