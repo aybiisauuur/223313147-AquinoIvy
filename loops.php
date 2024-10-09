b93d62cb358486d6eebf517bb8a12a8fef974314
aybiisauuur <ivyaquino981@gmail.com>
2024-10-09 21:56:13 +0800

    loops

---
+++ loops.php
@@ -0,0 +1,14 @@
+// activity 1
+
+$num1 = 1;
+while ($num1 <= 10) {
+    echo $num1, " ";
+    $num1++;
+}
+
+$numeven = 2;
+
+while ($numeven <= 20) {
+    echo $numeven, " ";
+    $numeven += 2;
+}
